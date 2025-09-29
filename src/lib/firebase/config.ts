946ce2668926bd506d9b89e92b94b092e53a800a
Laksh Bhutiya <lakshbhutiya122@gmail.com>
2025-09-29 16:59:25 +0000

    still showing the same

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,48 +1,21 @@
-// This file is intentionally left blank.
-// You should add your Firebase project configuration here.
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// For example:
-
-import { initializeApp, getApps } from "firebase/app";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAoM56hIeFuIi91s2EoroEbVc367TOlTcM",
-    authDomain: "ly-project-7308c.firebaseapp.com",
-    projectId: "ly-project-7308c",
-    storageBucket: "ly-project-7308c.firebasestorage.app",
-    messagingSenderId: "291915468031",
-    appId: "1:291915468031:web:a6ab5ef46fa1b12abc90ca",
-    measurementId: "G-ZCRTM3T9DN"
-  };
+  apiKey: "AIzaSyAoM56hIeFuIi91s2EoroEbVc367TOlTcM",
+  authDomain: "ly-project-7308c.firebaseapp.com",
+  projectId: "ly-project-7308c",
+  storageBucket: "ly-project-7308c.firebasestorage.app",
+  messagingSenderId: "291915468031",
+  appId: "1:291915468031:web:a6ab5ef46fa1b12abc90ca",
+  measurementId: "G-ZCRTM3T9DN"
+};
 
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-}
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export { app };
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAoM56hIeFuIi91s2EoroEbVc367TOlTcM",
-//   authDomain: "ly-project-7308c.firebaseapp.com",
-//   projectId: "ly-project-7308c",
-//   storageBucket: "ly-project-7308c.firebasestorage.app",
-//   messagingSenderId: "291915468031",
-//   appId: "1:291915468031:web:a6ab5ef46fa1b12abc90ca",
-//   measurementId: "G-ZCRTM3T9DN"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// export { app }
